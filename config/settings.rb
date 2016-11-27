66e19a20471ff63319abd19eb3af2df50636093f
Vlad Faust <vladislav.faust@gmail.com>
2016-11-27 19:54:29 +0300

    Temporary allowed everyone to add new sounds

--- config/settings.rb
+++ config/settings.rb
@@ -1,6 +1,6 @@
 module Settings
   MAX_SOUND_DURATION = 30.freeze # In seconds
-  ONLY_ADMIN_ADD_SOUND = true
+  ONLY_ADMIN_ADD_SOUND = false
   MAX_QUERY_RESULTS = 50.freeze
   HELLO_SOUND_ID = 32.freeze # John Cena!
   MAGIC_GIF_FILE = 'BQADAgADHAYAAitYbwAB3RefDXK4BxcC' # Thanks to Никита Кабаков (@metalnakls)
